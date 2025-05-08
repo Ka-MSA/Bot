a8b46a5bc04b0fdbf9d90360f0ca2bd1931c4600
Ka-MSA <kamsatech@gmail.com>
2025-05-08 21:39:59 +0200

    Update start_handler.py
    Update bot response

--- handlers/start_handler.py
+++ handlers/start_handler.py
@@ -1,4 +1,4 @@
 def register_start_handler(bot):
     @bot.message_handler(commands=['start'])
     def send_welcome(message):
-        bot.reply_to(message, "Welcome to the File Bot! Send /menu to see available sections or /get filename or /list to see all files.")
+        bot.reply_to(message, "Welcome to the Bot! Send /menu to see available sections.")
