e16c16af16374068761f973a54e031e39ce62632
Ka-MSA <kamsatech@gmail.com>
2025-05-08 21:12:06 +0200

    Update file_data.py
    Fixed sort function

--- file_data.py
+++ file_data.py
@@ -10,21 +10,30 @@ creds = service_account.Credentials.from_service_account_info(creds_dict)
 # Initialize Drive API
 drive_service = build('drive', 'v3', credentials=creds)
 
+# Replace this with your main folder ID
 MAIN_FOLDER_ID = '1-5ocbVU17S13rUgbaxi5kEWOXjAJWTsf'
 
 def get_subfolders(folder_id):
     query = f"'{folder_id}' in parents and mimeType='application/vnd.google-apps.folder' and trashed = false"
-    results = drive_service.files().list(q=query, fields="files(id, name)").execute()
+    results = drive_service.files().list(
+        q=query,
+        orderBy='name',
+        fields="files(id, name)"
+    ).execute()
     return results.get('files', [])
 
 def get_files_in_folder(folder_id):
     query = f"'{folder_id}' in parents and mimeType!='application/vnd.google-apps.folder' and trashed = false"
-    results = drive_service.files().list(q=query, fields="files(id, name)").execute()
+    results = drive_service.files().list(
+        q=query,
+        orderBy='name',
+        fields="files(id, name)"
+    ).execute()
     return results.get('files', [])
 
 def build_files_by_section():
     files_by_section = {}
-    subfolders = sorted(get_subfolders(MAIN_FOLDER_ID), key=lambda x: x['name'].lower())  # Sort sections
+    subfolders = get_subfolders(MAIN_FOLDER_ID)
 
     for folder in subfolders:
         section_name = folder['name']
@@ -37,9 +46,9 @@ def build_files_by_section():
             download_url = f"https://drive.google.com/uc?export=download&id={file_id}"
             files_dict[file_name] = download_url
 
-        sorted_files_dict = dict(sorted(files_dict.items()))  # Sort files in section
-        files_by_section[section_name] = sorted_files_dict
+        files_by_section[section_name] = files_dict
 
     return files_by_section
 
+# Exported dictionary
 files_by_section = build_files_by_section()
